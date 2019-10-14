b63ec9c722ce87e9ec5872a04606a0a63ae74359
esteffane <55981920+esteffane@users.noreply.github.com>
2019-10-14 08:23:07 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -3,7 +3,7 @@
 $login = $_POST['login'];
 $senha = MD5($_POST['senha']);
 $conexao = mysqli_connect ('nome_do_servidor','nome_de_usuario','senha');
-$db = mysql_select_db('cadastro');
+$dbname = mysql_select_db('cadastro');
 $query_select = "SELECT login FROM usuarios WHERE login = '$login'";
 $select = mysql_query($query_select,$conexao);
 $array = mysql_fetch_array($select);
