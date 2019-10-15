6fa00e0ca1300a20ca2a9136ce442f6bce2f67d7
esteffane <55981920+esteffane@users.noreply.github.com>
2019-10-15 09:33:20 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,7 +1,7 @@
 <?php
 	$servidor = "localhost:3307";
 	$usuario = "root";
-	$senha = "";
+	$senha = "labinfo";
 	$dbname = "cadastro";
 
 	$conexao = mysqli_connect($servidor, $usuario, $senha, $dbname)or die ('Não foi possível conectar');
